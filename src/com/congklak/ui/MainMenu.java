33dcb42d5e3f414d076c131288c24b60a1100983
Victor <angvictor91@gmail.com>
2020-06-27 15:05:37 +0700

    add imperative abstraction smell

--- src/com/congklak/ui/MainMenu.java
+++ src/com/congklak/ui/MainMenu.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class MainMenu {
 	public Scanner scan = new Scanner(System.in);
 	private GameMenu gameMenu = null;
-	private HowToPlayMenu howToPlayMenu = null;
 	
 	public MainMenu() {
 		int input = -1;
@@ -34,7 +33,7 @@ public class MainMenu {
 				case 3: gameMenu = new GameMenu(this, GameMenu.MEDIUM); break;
 				case 4: gameMenu = new GameMenu(this, GameMenu.HARD); break;
 				case 5: gameMenu = new GameMenu(this, GameMenu.EXPERT); break;
-				case 6: howToPlayMenu = new HowToPlayMenu(this); break;
+				case 6: printHowToPlay(); break;
 			}
 		} while(input != 7);
 	}
@@ -47,5 +46,19 @@ public class MainMenu {
 
 	public static void main(String[] args) {
 		new MainMenu();
+	}
+	
+	public void printHowToPlay() {
+		printLine();
+		System.out.println("How to Play\n" + 
+				"-----------\n" +
+				"Every player have 7 small holes and 1 big hole, every small hole have 7 units\n" + 
+				"Every turn, the Player must choose the hole to take the units. After that, the player must distribute to other holes by order and skip the opponent big hole\n" + 
+				"When the last unit drops to a filled small hole, then the player must take from the hole and distribute to other holes by order again\n" + 
+				"When the last unit drops to a big hole, then the player can choose the hole to the units again.\n" + 
+				"\n" + 
+				"When the last unit drops to the empty small hole, then the player must change turn.\n" + 
+				"When the last unit drops to the empty small hole on that's the player side, then the player takes the last unit and the unit from opponent small hole side to that's the player big hole.\n");
+		scan.nextLine();
 	}
 }
