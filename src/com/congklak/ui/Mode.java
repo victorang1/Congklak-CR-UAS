28565b0f2dd11e65e1f5f3ca3e097a73de83fc9c
Victor <angvictor91@gmail.com>
2020-06-27 14:51:54 +0700

    remove attribute mode abstract class (not used)

--- src/com/congklak/ui/Mode.java
+++ src/com/congklak/ui/Mode.java
@@ -2,9 +2,6 @@ package com.congklak.ui;
 
 public abstract class Mode {
 	
-	private String playerOneLabel;
-	private String playerTwoLabel;
-	
 	protected abstract String getPlayerOneLabel();
 	protected abstract String getPlayerTwoLabel();
 }
